bf2a97c3223ad61b39712118456537a33bf0f64b
franzqat <franz.f1032@gmail.com>
2017-02-12 16:54:10 +0100

    Add files via upload

--- items.lua
+++ items.lua
@@ -14,6 +14,21 @@ data:extend(
       condition_size = 4,
       condition = { "water-tile" }
     }
-   }
+   },
+   {
+    type = "item",
+    name = "hazard-concrete",
+    icon = "__base__/graphics/icons/hazard-concrete.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "terrain",
+    order = "b[concrete]-b[hazard]",
+    stack_size = 500,
+    place_as_tile =
+    {
+      result = "hazard-concrete-left",
+      condition_size = 4,
+      condition = { "water-tile" }
+    }
+  }
 })
 
